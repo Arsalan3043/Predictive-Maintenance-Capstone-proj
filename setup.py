b1feaea9d238aebbd3028424c4b5d2ae3fb3f6d6
Arsalan3043 <arsalankhanrkl@gmail.com>
2025-06-06 17:46:55 +0400

    logging and exception added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
-    description='A short description of the project.',
+    version='0.0.1',
+    description='An MLOps project for predictive maintenance',
     author='Md Arsalan Arshad',
     license='MIT',
 )
